8efa32a342f668a921701142154e60effaf157f8
SHIN-HANBEEN <shb0801@naver.com>
2023-11-15 21:06:04 +0900

    init commit

--- js/a.js
+++ js/a.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navItems = document.querySelectorAll(".nav-item");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const navItems = document.querySelectorAll(".nav-item");
 
-  navItems.forEach((navItem, i) => {
-    navItem.addEventListener("click", () => {
-      navItems.forEach((item, j) => {
-        item.classList.remove("active");
-      });
-      navItem.classList.add("active");
+//   navItems.forEach((navItem, i) => {
+//     navItem.addEventListener("click", () => {
+//       navItems.forEach((item, j) => {
+//         item.classList.remove("active");
+//       });
+//       navItem.classList.add("active");
+//     });
+//   });
+// });
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach((item, j) => {
+      item.className = "nav-item";
     });
+    navItem.className = "nav-item active";
   });
 });
